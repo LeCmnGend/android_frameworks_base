6dd5bc0ec4e369320ffdbcf2f0c543575464e963
arthurhung <arthurhung@google.com>
2020-07-23 17:15:04 +0800

    Fix WindowInputTests#testOverlapWindow failing (1/2)
    The 'InputWindowInfo.isTrustedOverlay' function had been removed, we
    have to indicate the trustedOverlay flag when fill input info to overlay
    window.
    
    Bug: 161789401
    Bug: 158717144
    Test: atest WindowInputTests
    Change-Id: Idf17fd64c6a1399d04c51e3f0bd600dc4a84239a
    Merged-In: Idf17fd64c6a1399d04c51e3f0bd600dc4a84239a
    (cherry picked from commit on googleplex-android-review.googlesource.com host: 657c2a411593ffb95c7f91a972bbe84f1a2df790)
    Merged-In: Idf17fd64c6a1399d04c51e3f0bd600dc4a84239a

--- services/core/java/com/android/server/wm/WindowState.java
+++ services/core/java/com/android/server/wm/WindowState.java
@@ -956,17 +956,7 @@ class WindowState extends WindowContainer<WindowState> implements WindowManagerP
         mInputWindowHandle.trustedOverlay =
                 (mAttrs.privateFlags & PRIVATE_FLAG_TRUSTED_OVERLAY) != 0
                 && mOwnerCanAddInternalSystemWindow;
-        mInputWindowHandle.trustedOverlay |=
-                mAttrs.type == TYPE_ACCESSIBILITY_MAGNIFICATION_OVERLAY
-                || mAttrs.type == TYPE_INPUT_METHOD || mAttrs.type == TYPE_INPUT_METHOD_DIALOG
-                || mAttrs.type == TYPE_MAGNIFICATION_OVERLAY || mAttrs.type == TYPE_STATUS_BAR
-                || mAttrs.type == TYPE_NOTIFICATION_SHADE
-                || mAttrs.type == TYPE_NAVIGATION_BAR
-                || mAttrs.type == TYPE_NAVIGATION_BAR_PANEL
-                || mAttrs.type == TYPE_SECURE_SYSTEM_OVERLAY
-                || mAttrs.type == TYPE_DOCK_DIVIDER
-                || mAttrs.type == TYPE_ACCESSIBILITY_OVERLAY
-                || mAttrs.type == TYPE_INPUT_CONSUMER;
+        mInputWindowHandle.trustedOverlay |= InputMonitor.isTrustedOverlay(mAttrs.type);
 
         // Make sure we initial all fields before adding to parentWindow, to prevent exception
         // during onDisplayChanged.
