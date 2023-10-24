fa42315bfe4289633701f2b611f9bab27f80d094
Tim Josten <timjosten@gmail.com>
2023-10-24 21:55:02 +0700

    PixelPropUtils: Fix GMS drain triggered by an outdated ROM build date
    If the build date exceeds a month, GMS, thinking the device is Pixel, attempts a system update, which unexpectedly fails. This goes into an endless cycle which drains battery very quickly and generates a lot of heat. Let's fix it by spoofing the build date to something always fresh.

--- core/java/com/android/internal/util/syberia/PixelPropsUtils.java
+++ core/java/com/android/internal/util/syberia/PixelPropsUtils.java
@@ -172,6 +172,7 @@ public class PixelPropsUtils {
             return;
         }
         if (packageName.equals("com.google.android.gms")) {
+            setPropValue("TIME", System.currentTimeMillis());
             final String processName = Application.getProcessName();
             if (processName.equals("com.google.android.gms.unstable")) {
                 sIsGms = true;
@@ -213,6 +214,7 @@ public class PixelPropsUtils {
                 setPropValue(key, value);
             }
             if (packageName.equals("com.google.android.gms")) {
+                setPropValue("TIME", System.currentTimeMillis());
                 final String processName = Application.getProcessName();
                 if (processName.equals("com.google.android.gms.unstable")) {
                     sIsGms = true;
