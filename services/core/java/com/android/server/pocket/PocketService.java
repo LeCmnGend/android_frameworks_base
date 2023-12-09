50341467f19b53acb6b629331183836d41f8be10
LeCmnGend <namhoang1235@gmail.com>
2023-12-09 09:40:59 +0700

    Revert "base: Pocket Judge: Don't block dialer UI"
    This reverts commit 5eef3a499a051042d79716c3652c0ac3b5941e4b.
    
    Dialer needs to block

--- services/core/java/com/android/server/pocket/PocketService.java
+++ services/core/java/com/android/server/pocket/PocketService.java
@@ -39,8 +39,6 @@ import android.pocket.IPocketCallback;
 import android.pocket.PocketConstants;
 import android.pocket.PocketManager;
 import android.provider.Settings.System;
-import android.telephony.PhoneStateListener;
-import android.telephony.TelephonyManager;
 import android.text.TextUtils;
 import android.util.Log;
 import android.util.Slog;
@@ -129,15 +127,13 @@ public class PocketService extends SystemService implements IBinder.DeathRecipie
      */
     private static final float POCKET_LIGHT_MAX_THRESHOLD = 3.0f;
 
-    private final ArrayList<IPocketCallback> mCallbacks = new ArrayList<>();
-    private final TelephonyManager mTelephonyManager;
+    private final ArrayList<IPocketCallback> mCallbacks= new ArrayList<>();
 
     private Context mContext;
     private boolean mEnabled;
     private boolean mSystemReady;
     private boolean mSystemBooted;
     private boolean mInteractive;
-    private boolean mIsOnCall;
     private boolean mPending;
     private PocketHandler mHandler;
     private PocketObserver mObserver;
@@ -187,17 +183,11 @@ public class PocketService extends SystemService implements IBinder.DeathRecipie
         }
         mVendorSensor = getSensor(mSensorManager, mVendorPocketSensor);
         mSupportedByDevice = mContext.getResources().getBoolean(
-                com.android.internal.R.bool.config_pocketModeSupported);
+                                 com.android.internal.R.bool.config_pocketModeSupported);
         mObserver = new PocketObserver(mHandler);
         if (mSupportedByDevice){
             mObserver.onChange(true);
             mObserver.register();
-        }
-
-        mTelephonyManager = (TelephonyManager)
-                context.getSystemService(Context.TELEPHONY_SERVICE);
-        if (mSupportedByDevice) {
-            mTelephonyManager.listen(mOnCallStateListener, PhoneStateListener.LISTEN_CALL_STATE);
         }
     }
 
@@ -231,17 +221,6 @@ public class PocketService extends SystemService implements IBinder.DeathRecipie
             }
         }
 
-    }
-
-    final PhoneStateListener mOnCallStateListener = new OnCallStateListener();
-    private class OnCallStateListener extends PhoneStateListener {
-        @Override
-        public void onCallStateChanged(int state, String incomingNumber) {
-            final boolean call = state != TelephonyManager.CALL_STATE_IDLE;
-            final boolean changed = call != mIsOnCall;
-            mIsOnCall = call;
-            if (changed) update();
-        }
     }
 
     private class PocketHandler extends Handler {
@@ -348,7 +327,6 @@ public class PocketService extends SystemService implements IBinder.DeathRecipie
         }
         unregisterSensorListeners();
         mObserver.unregister();
-        mTelephonyManager.listen(mOnCallStateListener, PhoneStateListener.LISTEN_NONE);
     }
 
     private final class PocketServiceWrapper extends IPocketService.Stub {
@@ -509,8 +487,8 @@ public class PocketService extends SystemService implements IBinder.DeathRecipie
         if (!mSupportedByDevice){
             return;
         }
-        if (!mEnabled || mInteractive || mIsOnCall) {
-            if (mEnabled && isDeviceInPocket() && !mIsOnCall) {
+        if (!mEnabled || mInteractive) {
+            if (mEnabled && isDeviceInPocket()) {
                 // if device is judged to be in pocket while switching
                 // to interactive state, we need to keep monitoring.
                 return;
@@ -519,7 +497,6 @@ public class PocketService extends SystemService implements IBinder.DeathRecipie
         } else {
             mHandler.removeMessages(PocketHandler.MSG_UNREGISTER_TIMEOUT);
             registerSensorListeners();
-            handleDispatchCallbacks();
         }
     }
 
--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -5232,11 +5232,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             return;
         }
 
-        TelecomManager telecomManager = getTelecommService();
-        if (telecomManager != null && telecomManager.isInCall()) {
-            return;
-        }
-
         if (mPowerManager.isInteractive() && !isKeyguardShowingAndNotOccluded()){
             return;
         }
